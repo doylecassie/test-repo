634d03d4b30cfea0ca5d93ff51a9b7b4b5aff4d3
Cassie Doyle <cassie.doyle@live.ca>
2019-01-29 19:17:52 -0500

    Theme set up
    all changes

---
+++ functions.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * cassieunderscore functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package cassieunderscore
+ */
+
+/**
+ * Theme setup.
+ */
+require get_template_directory() . '/inc/setup.php';
+
+/**
+ * Enqueue scripts and styles.
+ */
+function cassieunderscore_scripts() {
+	wp_enqueue_style( 'cassieunderscore-style', get_stylesheet_uri() );
+
+	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+		wp_enqueue_script( 'comment-reply' );
+	}
+}
+add_action( 'wp_enqueue_scripts', 'cassieunderscore_scripts' );
+
+
+/**
+ * Custom template tags for this theme.
+ */
+require get_template_directory() . '/inc/template-tags.php';
+
+/**
+ * Functions which enhance the theme by hooking into WordPress.
+ */
+require get_template_directory() . '/inc/template-functions.php';
+
+/**
+ * Customizer additions.
+ */
+require get_template_directory() . '/inc/customizer.php';
+
+/**
+ * Load Jetpack compatibility file.
+ */
+if ( defined( 'JETPACK__VERSION' ) ) {
+	require get_template_directory() . '/inc/jetpack.php';
+}
+
